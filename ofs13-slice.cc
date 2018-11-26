3f741b9053e73cc4da836e1595c4e9010d177b6a
Pedro <31418730+pedrobellotti@users.noreply.github.com>
2018-11-26 11:43:41 -0200

    mudanças na conexao

--- ofs13-slice.cc
+++ ofs13-slice.cc
@@ -42,13 +42,6 @@
 
 using namespace ns3;
 
-void regra (Ptr<OFSwitch13Controller> c){
-  c->DpctlExecute(1, "flow-mod cmd=add,table=0,prio=102 "
-                "in_port=2 apply:output=10");
-  c->DpctlExecute(1, "flow-mod cmd=add,table=0,prio=101 "
-                "in_port=10 apply:output=2");
-}
-
 int
 main (int argc, char *argv[])
 {
@@ -59,12 +52,12 @@ main (int argc, char *argv[])
   uint16_t numberOfHosts = 10;
   //uint16_t numberOfServers = 4;
 
-
   // Configure command line parameters
   CommandLine cmd;
   cmd.AddValue ("simTime", "Simulation time (seconds)", simTime);
   cmd.AddValue ("verbose", "Enable verbose output", verbose);
   cmd.AddValue ("trace", "Enable datapath stats and pcap traces", trace);
+  cmd.AddValue ("numberOfHosts", "Number of hosts", numberOfHosts);
   cmd.Parse (argc, argv);
 
   if (verbose)
@@ -96,27 +89,41 @@ main (int argc, char *argv[])
   csmaHelper.SetChannelAttribute ("DataRate", DataRateValue (DataRate ("100Mbps")));
   csmaHelper.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (2)));
 
-  NetDeviceContainer hostGroup0;
-  NetDeviceContainer hostGroup1;
+  NetDeviceContainer roteador0;
+  NetDeviceContainer roteador1;
+  NodeContainer hostGroup0;
+  NodeContainer hostGroup1;
   NetDeviceContainer switchPorts;
 
   /* Dividindo os dois grupos de Hosts
   * Ainda não tentamos fazer com os servidores, somente conectar os dois grupos de host
   */
   for (size_t i = 0; i < hosts.GetN ()/2; i++)
-    {
-      NodeContainer pair (hosts.Get (i), switchNode);
-      NetDeviceContainer link = csmaHelper.Install (pair);
-      hostGroup0.Add (link.Get (0));
-      switchPorts.Add (link.Get (1));
-    }
+      hostGroup0.Add (hosts.Get (i));
+  roteador0 = csmaHelper.Install(hostGroup0);
+
   for (size_t i = hosts.GetN ()/2; i < hosts.GetN (); i++)
-    {
-      NodeContainer pair (hosts.Get (i), switchNode);
-      NetDeviceContainer link = csmaHelper.Install (pair);
-      hostGroup1.Add (link.Get (0));
-      switchPorts.Add (link.Get (1));
-    }
+      hostGroup1.Add (hosts.Get (i));
+  roteador1 = csmaHelper.Install(hostGroup1);
+
+  /*
+  * Conecta os roteadores no switch (não funciona)
+  */
+  NodeContainer pair (hostGroup0.Get(1), switchNode);
+  NetDeviceContainer link = csmaHelper.Install(pair);
+  switchPorts.Add(link.Get(1));
+
+  NodeContainer pair1 (hostGroup1.Get(4), switchNode);
+  NetDeviceContainer link1 = csmaHelper.Install(pair1);
+  switchPorts.Add(link1.Get(1));
+  
+  /*NodeContainer par;
+  NetDeviceContainer pairDevs;
+  par = NodeContainer (hostGroup0, hostGroup1);
+  pairDevs = csmaHelper.Install (par);
+  switchPorts.Add (pairDevs.Get (0));
+  switchPorts.Add (pairDevs.Get (1));*/
+    
   // Create the controller node
   Ptr<Node> controllerNode = CreateObject<Node> ();
 
@@ -136,12 +143,12 @@ main (int argc, char *argv[])
   Ipv4InterfaceContainer hostIpIfacesGroup0;
   Ipv4InterfaceContainer hostIpIfacesGroup1;
   ipv4helpr.SetBase ("10.1.1.0", "255.255.255.0");
-  hostIpIfacesGroup0 = ipv4helpr.Assign (hostGroup0);
-  ipv4helpr.SetBase ("10.1.2.0", "255.255.255.0"); //Se comentar essa linha, funciona normalmente. Com ips diferentes não funciona
-  hostIpIfacesGroup1 = ipv4helpr.Assign (hostGroup1);
+  hostIpIfacesGroup0 = ipv4helpr.Assign (roteador0);
+  ipv4helpr.SetBase ("10.1.2.0", "255.255.255.0");
+  hostIpIfacesGroup1 = ipv4helpr.Assign (roteador1);
 
   // Aplicação bulk-send
-  BulkSendHelper clienteC1 ("ns3::TcpSocketFactory",
+  /*BulkSendHelper clienteC1 ("ns3::TcpSocketFactory",
                            InetSocketAddress (hostIpIfacesGroup0.GetAddress (1), 9));
   clienteC1.SetAttribute ("MaxBytes", UintegerValue (0));
   ApplicationContainer c1Apps = clienteC1.Install (hosts.Get (9));
@@ -152,28 +159,19 @@ main (int argc, char *argv[])
                           InetSocketAddress (Ipv4Address::GetAny (), 9));
   ApplicationContainer s1Apps = sinkS1.Install (hosts.Get (1));
   s1Apps.Start (Seconds (1.0));
-  s1Apps.Stop (Seconds (10.0));
+  s1Apps.Stop (Seconds (10.0));*/
 
-  // Aplicação ping (mesmo problema da bulk-send)
 
-  /*// Configure ping application between hosts
+  // Configure ping application between hosts
   V4PingHelper pingHelper = V4PingHelper (hostIpIfacesGroup0.GetAddress (1));
   pingHelper.SetAttribute ("Verbose", BooleanValue (true));
   ApplicationContainer pingApps = pingHelper.Install (hosts.Get (9));
-  pingApps.Start (Seconds (1));*/
+  pingApps.Start (Seconds (1));
 
   //Imprime as tabelas
-  Simulator::Schedule (Seconds(9.9), &OFSwitch13Device::PrintFlowTables, switches.Get(0));
-  //Adiciona as regras manualmente
-  Simulator::Schedule (Seconds(0.9), &regra, controller);
+  //Simulator::Schedule (Seconds(9.9), &OFSwitch13Device::PrintFlowTables, switches.Get(0));
 
-  /* Os comandos acima foram para testar o porque não estar funcionando
-      1- Com IPs diferentes, o controlador não adiciona nenhuma regra no switch
-      2- Com IPs iguais, adiciona a regra
-      3- Tentamos adicionar as mesmas regras manualmente no switch com IPs diferentes, mas mesmo assim não funcionou
-   */
-
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables (); //Mesmo com esse comando não funcionou
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
   // Enable datapath stats and pcap traces at hosts, switch(es), and controller(s)
   if (trace)
@@ -190,6 +188,6 @@ main (int argc, char *argv[])
   Simulator::Destroy ();
 
   //Resultados do bulk-send
-  Ptr<PacketSink> sink1 = DynamicCast<PacketSink> (s1Apps.Get (0));
-  std::cout << "(S1) Total Bytes Received: " << sink1->GetTotalRx () << std::endl;
+  //Ptr<PacketSink> sink1 = DynamicCast<PacketSink> (s1Apps.Get (0));
+  //std::cout << "(S1) Total Bytes Received: " << sink1->GetTotalRx () << std::endl;
 }
