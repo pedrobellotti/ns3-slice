5f1c0bdd81a65205b0c18d1a11e93b9ba75f70a9
Pedro <31418730+pedrobellotti@users.noreply.github.com>
2019-02-21 11:23:32 -0300

    atualizando script simulação

--- Scripts/sim.sh
+++ Scripts/sim.sh
@@ -6,21 +6,16 @@ cont=0
 hs1=( 50 100 200 300 400 500 600) #Numero de hosts slice 1
 hs2=( 50 100 200 300 400 500 600) #Numero de hosts slice 2
 len=${#hs1[@]}
-rng=2
+rng=$1
 
-while [ $rng -lt 5 ]
-	    do
-		while [ $cont -lt $len ]
-		    do
-			echo "Esperando 3 segundos para começar a simulação"
-			sleep 3s
-			echo
-			echo "Começando simulação - RNG = $rng"
-			./waf --run "slice2 --RngRun=$rng --trace=true --simTime=700 --hostsSlice1=${hs1[$cont]} --hostsSlice2=${hs2[$cont]}"
-			echo "Simulação finalizada"
-			echo
-			cont=$(( cont+1 ))
-		done
-		cont=0
-		rng=$(( rng+1 ))
+while [ $cont -lt $len ]
+	do
+		echo "Esperando 3 segundos para começar a simulação"
+		sleep 3s
+		echo
+		echo "Começando simulação - RNG = $rng"
+		./waf --run "slice2 --RngRun=$rng --trace=true --simTime=700 --hostsSlice1=${hs1[$cont]} --hostsSlice2=${hs2[$cont]}"
+		echo "Simulação finalizada"
+		echo
+		cont=$(( cont+1 ))
 done
